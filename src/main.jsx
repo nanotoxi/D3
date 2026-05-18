160a01d80c6e18297ae3500aa3a764678f4cfa3c
nanotoxi <contact@nanotoxi.com>
2026-05-18 14:04:44 +0530

    Add www -> nanotoxi.com redirect before app renders

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Redirect www -> apex before rendering anything
+if (window.location.hostname === 'www.nanotoxi.com') {
+  window.location.replace(
+    window.location.href.replace('//www.nanotoxi.com', '//nanotoxi.com')
+  )
+} else {
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+}
